fa988663cad89294a64711858f0def7c9fe3c4fc
Kumar Shashank <1711shashank@gmail.com>
2023-06-11 00:04:19 +0530

    change in file stracture

--- src/components/RegistrationHeader.js
+++ src/components/RegistrationHeader.js
@@ -8,7 +8,7 @@ const RegistrationHeader = () => {
                     <img src='https://inceptra-2023.onrender.com/static/media/InceptraLogo.25e3b540e4e346c3dd8c.png' alt='College Logo' />
                 </div>
                 <div className='dept-name'>
-                    <p className='main-text'> Department of IT</p>
+                    <p className='main-text'> Department of IT, NSEC</p>
                     <p className='sub-text'> Connecting Alumni</p>
                 </div>
                 <div className='logo'>
